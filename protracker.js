3e8a3a2eb4af9a54bc251b184d6af2b267f64b6c
adopth <developer@adop.co.th>
2025-10-03 19:09:47 +0700

    Update protracker.js 18.2.0

--- protracker.js
+++ protracker.js
@@ -4,7 +4,7 @@
  */
 (function() {
     // To update in the future, just change the version number here
-    var coreTrackerVersion = 'v19.0.0'; 
+    var coreTrackerVersion = 'v18.2.0'; 
 
     var script = document.createElement('script');
     script.async = true;
